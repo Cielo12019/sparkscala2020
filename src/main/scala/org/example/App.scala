114c2aa7ff5c86629a71825b0d6599e16202f3c7
Cielo Escobedo <cieloescobedo20@gmail.com>
2023-04-12 03:26:20 -0500

    TO -BE

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -4,19 +4,13 @@ package org.example
  * @author ${user.name}
  */
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.Dataset
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.Encoder
-import org.apache.spark.sql.Encoders
+
 
 
 object App{
-  //System.setProperty("hadoop.home.dir","c:\\winutils")
   val spark = SparkSession.builder()
     .master("local[1]")
     .appName("SparkByExample")
-    //.enableHiveSupport()
     .getOrCreate();
 
   println("First SparkContext:")
